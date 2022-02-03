682b731ade655463e86da400f37376767f8b2441
shireen30 <70129608+shireen30@users.noreply.github.com>
2022-02-03 11:45:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
+
+
+
 //var
 var r01, R01_move, R01_die
-var block1
-var block2
-var block3
-var block4
 
+var blockGroup;
 var scrapperImg;
 var scrapper;
 // Preload
@@ -20,8 +20,8 @@ function preload()
 // Setup
 function setup()
 {
-  createCanvas(windowWidth, windowHeight)
-  
+createCanvas(windowWidth, windowHeight)
+blockGroup=new Group();
 r01 = createSprite(500,500,50,50)
 r01.addAnimation("moving", R01_move);
 r01.scale=0.3
@@ -31,10 +31,8 @@ r01.setCollider("rectangle",0,0,400,400)
 
 scrapper=createSprite(width/2,250,width,300);
 scrapper.addAnimation("moving",scrapperImg);
-block1 = createSprite(Math.round(random(0,width)), height-20, 400, 40)
-block1.velocityY=-3
-block1.shapeColor="black"
-block1.debug=false
+
+
 
 }
 
@@ -59,15 +57,28 @@ if (keyDown("RIGHT")){
   r01.addAnimation("moving", R01_move);
 }
 
-console.log(mouseX, mouseY)
+//console.log(mouseX, mouseY)
 
 if(keyDown("space")){
   r01.velocityY=-10;
 }
 
-r01.velocityY+=0.8;
-
-r01.collide(block1)
+r01.velocityY+=0.1;
+//camera.position.y=r01.position.y
+r01.collide(blockGroup)
+spawnBlocks();
 
 }
 
+function spawnBlocks(){
+  if(frameCount%130==0){
+
+ var block1 = createSprite(Math.round(random(300,width/2+200)),Math.round(random(r01.y+100,r01.y+400)),350,50)
+ console.log(block1.x,block1.y) 
+ block1.velocityY=-1
+  block1.shapeColor="black"
+  block1.debug=true
+  blockGroup.add(block1);
+ 
+  }
+}
